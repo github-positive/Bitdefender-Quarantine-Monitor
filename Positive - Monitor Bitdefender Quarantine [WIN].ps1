abb476f35ea4d2320213de843e5e411a62c24d86
github-positive <165818323+github-positive@users.noreply.github.com>
2025-01-08 11:35:47 -0500

    Update Positive - Monitor Bitdefender Quarantine [WIN].ps1

--- Positive - Monitor Bitdefender Quarantine [WIN].ps1
+++ Positive - Monitor Bitdefender Quarantine [WIN].ps1
@@ -16,88 +16,63 @@ if ($Version -lt "6.3") {
     exit 1
 }
 $SQLLitePath = "$env:ProgramData\System.Data.SQLite.dll"
-$SQLLitePresent = Test-Path $SQLLitePath
+$SQLLiteOldPath = "$env:ProgramData\System.Data.SQLite-Old.dll"
 $alertThreshold = (Get-Date).AddDays(-1)
 $diagMessages = @()
 
-if (!$SQLLitePresent) {
+if ((Test-Path $SQLLitePath) -and (Test-Path $SQLLiteOldPath)) {
     try {
-        $DownloadUrl = "https://system.data.sqlite.org/blobs/1.0.119.0/sqlite-netFx46-binary-bundle-x64-2015-1.0.119.0.zip"
-        $TempZipFile = "$env:TEMP\sqlite-netFx46-binary-bundle-x64.zip"
-        $ExtractPath = "$env:TEMP\SQLite"
-        $SQLiteDllPath = Join-Path $ExtractPath "System.Data.SQLite.dll"
-
-        Invoke-WebRequest -Uri $DownloadUrl -OutFile $TempZipFile -UseBasicParsing
-
-        if (Test-Path $ExtractPath) {
-            Remove-Item -Path $ExtractPath -Recurse -Force
+        add-type -path $SQLLitePath
+    }
+    catch {
+        try {
+            add-type -path $SQLLiteOldPath
         }
-        Expand-Archive -Path $TempZipFile -DestinationPath $ExtractPath -Force
-
-        if (-not (Test-Path $SQLiteDllPath)) {
-            write-DRMMDiag "ERROR: No SQLite DLL. Please run 'Positive - Add Bitdefender Quarantine Reader [WIN]' to add the DLL file."
-            write-DRMMAlert "ERROR: No SQLite DLL. Please run 'Positive - Add Bitdefender Quarantine Reader [WIN]' to add the DLL file."
+        catch {
+            write-DRMMDiag "Could not load database components."
+            write-DRMMAlert "Could not load database components."
             exit 1
-        } else {
-            Copy-Item -Path $SQLiteDllPath -Destination $SQLLitePath
         }
-    } catch {
-        write-DRMMDiag "ERROR: No SQLite DLL. Please run 'Positive - Add Bitdefender Quarantine Reader [WIN]' to add the DLL file."
-        write-DRMMAlert "ERROR: No SQLite DLL. Please run 'Positive - Add Bitdefender Quarantine Reader [WIN]' to add the DLL file."
+    }
+    $con = New-Object -TypeName System.Data.SQLite.SQLiteConnection
+    $con.ConnectionString = "Data Source=$env:systemdrive\Program Files\Bitdefender\Endpoint Security\Quarantine\cache.db"
+    $con.Open()
+    $sql = $con.CreateCommand()
+    $sql.CommandText = "select * from entries"
+    $adapter = New-Object -TypeName System.Data.SQLite.SQLiteDataAdapter $sql
+    $data = New-Object System.Data.DataSet
+    [void]$adapter.Fill($data)
+    $sql.Dispose()
+    $con.Close()
+    
+    $CurrentQ = foreach ($row in $Data.Tables.rows) {
+        $quarantinedOn = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($row.quartime))
+        $lastAccessedOn = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($row.acctime))
+        $lastModifiedOn = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($row.modtime))
+        
+        # Check if any of the dates are within the date range
+        if ($quarantinedOn -gt $alertThreshold -or $lastAccessedOn -gt $alertThreshold -or $lastModifiedOn -gt $alertThreshold) {
+            [PSCustomObject]@{
+                Path               = $row.path
+                Threat             = $row.threat
+                Size               = $row.Size
+                'Quarantined On'   = $quarantinedOn
+                'Last accessed On' = $lastAccessedOn
+                'Last Modified On' = $lastModifiedOn
+            }
+        }
+    }
+    
+    if ($CurrentQ) {
+        $diagMessages += $CurrentQ
+        write-DRMMDiag $diagMessages
+        write-DRMMAlert "Unhealthy - $($CurrentQ.Count) Quarantine files found within the last $alertThreshold days. See Diagnostic data."
         exit 1
     }
-}
-
-$SQLLitePresent = Test-Path $SQLLitePath
-if (!$SQLLitePresent) {
-    write-DRMMDiag "ERROR: No SQLite DLL. Please run 'Positive - Add Bitdefender Quarantine Reader [WIN]' to add the DLL file."
-    write-DRMMAlert "ERROR: No SQLite DLL. Please run 'Positive - Add Bitdefender Quarantine Reader [WIN]' to add the DLL file."
-    exit 1
-}
-
-try {
-    add-type -path $SQLLitePath
-}
-catch {
-    write-DRMMDiag "Could not load database components."
-    write-DRMMAlert "Could not load database components."
-    exit 1
-}
-$con = New-Object -TypeName System.Data.SQLite.SQLiteConnection
-$con.ConnectionString = "Data Source=C:\Program Files\Bitdefender\Endpoint Security\Quarantine\cache.db"
-$con.Open()
-$sql = $con.CreateCommand()
-$sql.CommandText = "select * from entries"
-$adapter = New-Object -TypeName System.Data.SQLite.SQLiteDataAdapter $sql
-$data = New-Object System.Data.DataSet
-[void]$adapter.Fill($data)
-$sql.Dispose()
-$con.Close()
-
-$CurrentQ = foreach ($row in $Data.Tables.rows) {
-    $quarantinedOn = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($row.quartime))
-    $lastAccessedOn = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($row.acctime))
-    $lastModifiedOn = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($row.modtime))
-    
-    # Check if any of the dates are within the date range
-    if ($quarantinedOn -gt $alertThreshold -or $lastAccessedOn -gt $alertThreshold -or $lastModifiedOn -gt $alertThreshold) {
-        [PSCustomObject]@{
-            Path               = $row.path
-            Threat             = $row.threat
-            Size               = $row.Size
-            'Quarantined On'   = $quarantinedOn
-            'Last accessed On' = $lastAccessedOn
-            'Last Modified On' = $lastModifiedOn
-        }
+    else {
+        write-DRMMAlert "Healthy - No infections found."
     }
-}
-
-if ($CurrentQ) {
-    $diagMessages += $CurrentQ
-    write-DRMMDiag $diagMessages
-    write-DRMMAlert "Unhealthy - $($CurrentQ.Count) Quarantine files found within the last $alertThreshold days. See Diagnostic data."
-    exit 1
-}
-else {
-    write-DRMMAlert "Healthy - No infections found."
+} else {
+    #Do nothing, we have another monitor/response for this
+    write-DRMMAlert "SQLite DLL files missing."
 }
